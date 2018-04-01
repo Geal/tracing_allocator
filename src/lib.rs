e5bcb1345a25ea97e7ea55c4c41b1e987d05e087
Geoffroy Couprie <geo.couprie@gmail.com>
2018-04-01 20:28:33 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 
 extern crate libc;
 
-
 use std::heap::{Alloc, System, Layout, AllocErr};
 use std::fs::File;
 use std::mem;
@@ -41,64 +40,78 @@ fn to_hex(i: u8) -> u8 {
   }
 }
 
+enum Action {
+  Allocating,
+  Deallocating
+}
+
+unsafe fn print_size(address: usize, size: usize, action: Action) {
+  if TRACE_FD != 0 {
+    let mut buf: [u8; 38] = mem::uninitialized();
+    match action {
+      Action::Allocating => buf[0] = b'A',
+      Action::Deallocating => buf[0] = b'D',
+    };
+
+    buf[1] = b' ';
+
+    let psz = mem::size_of::<usize>();
+    let mut counter = 8;
+    let mut index = 2;
+
+    let shr = (psz as u32)*8 - 8;
+
+    let c = address;
+    loop {
+      if counter == 0 {
+        break;
+      }
+
+      let current = ((c.overflowing_shl(64 - 8*counter as u32).0).overflowing_shr(shr).0) as u8;
+      buf[index] = to_hex(current >> 4);
+      buf[index+1] = to_hex( (current << 4) >> 4);
+
+      counter -= 1;
+      index += 2;
+    }
+
+    buf[index] = b' ';
+    index += 1;
+
+    counter = 8;
+    loop {
+      if counter == 0 {
+        break;
+      }
+
+      let current = ((size.overflowing_shl(64 - 8*counter as u32).0).overflowing_shr(shr).0) as u8;
+      buf[index] = to_hex(current >> 4);
+      buf[index+1] = to_hex( (current << 4) >> 4);
+
+      counter -= 1;
+      index += 2;
+    }
+    buf[index] = b'\n';
+
+    libc::write(TRACE_FD,
+      buf.as_ptr() as *const c_void,
+      buf.len());
+  }
+
+}
+
 unsafe impl<'a> Alloc for &'a Allocator {
   unsafe fn alloc(&mut self, layout: Layout) -> Result<*mut u8, AllocErr> {
     let size = layout.size();
     let res = System.alloc(layout);
     if let Ok(p) = res {
-      if TRACE_FD != 0 {
-        let mut buf: [u8; 35] = unsafe { mem::uninitialized() };
-
-        let psz = mem::size_of::<usize>();
-        let mut counter = 8;
-        let mut index = 0;
-
-        let shr = (psz as u32)*8 - 8;
-
-        let c: usize = p as usize;
-        loop {
-          if counter == 0 {
-            break;
-          }
-
-          let current = ((c.overflowing_shl(64 - 8*counter as u32).0).overflowing_shr(shr).0) as u8;
-          buf[index] = to_hex(current >> 4);
-          buf[index+1] = to_hex( (current << 4) >> 4);
-
-          counter -= 1;
-          index += 2;
-        }
-
-        buf[index] = b':';
-        buf[index+1] = b' ';
-        index += 2;
-
-        counter = 8;
-        loop {
-          if counter == 0 {
-            break;
-          }
-
-          let current = ((size.overflowing_shl(64 - 8*counter as u32).0).overflowing_shr(shr).0) as u8;
-          buf[index] = to_hex(current >> 4);
-          buf[index+1] = to_hex( (current << 4) >> 4);
-
-          counter -= 1;
-          index += 2;
-        }
-        buf[index] = b'\n';
-
-        unsafe {
-          libc::write(TRACE_FD,
-            buf.as_ptr() as *const c_void,
-            buf.len())
-        };
-      }
+      print_size(p as usize, size, Action::Allocating);
     }
     res
   }
 
   unsafe fn dealloc(&mut self, ptr: *mut u8, layout: Layout) {
+    print_size(ptr as usize, layout.size(), Action::Deallocating);
     System.dealloc(ptr, layout)
   }
 }
